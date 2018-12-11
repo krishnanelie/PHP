f38e9118aab4338658397ecedd5ef10f6e03c33b
Krishna Neliê <krishna.nelie@gmail.com>
2018-12-11 00:17:58 -0200

    Fazendo uma calculadora PHP.

---
+++ calculadora.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>Minha Calculadora em PHP</title>
+</head>
+<body>
+<form method = "post" action = "calcular.php">
+Valor 1:
+<input type = "text" name = "valor1" size = "5"/>
+<select name = "tipo">
+<option select = " selected" > Somar </option>
+<option> Subtrair </option>
+<option> Multiplicar </option>
+<option> Dividir </option>
+</select>
+Valor 2:
+<input type = "text" name = "valor2" size = "5" />
+<input type = "Submit" name = "calcularbtn" value = "Calcular"/>
+</form>
+</body>
+</html>
+
